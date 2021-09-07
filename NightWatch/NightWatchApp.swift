2f4388e1f7355c8717cad40c3f7fe4a03c053171
Miguel <miguel.rueda@outlook.com>
2021-09-06 21:18:25 -0500

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Miguel Rueda on 06/09/21.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
